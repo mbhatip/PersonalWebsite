aee8c4a2298540b3fa5efdda9a0c49192dcf2675
juneadkhan <junead@live.unc.edu>
2021-02-24 00:02:33 -0500

    Added Navigation Bar

--- main.js
+++ main.js
@@ -1,24 +1,77 @@
 export const loadDOM = function() {
     const $root = $('#root');
-    $root.append(mainBanner)
-
-    
+    $root.append(navBar)
+    $root.append(heroSection) 
 };
 
-export const mainBanner = function() {
+export const navBar = function() {
     return `
-    <section class="hero is-dark is-fullheight">
-    <div class="hero-body">
-        <div class="containe">
-            <h1 class="title">
-                Junead Khan
-            </h1>
-            <h2 class="subtitle">
-                Personal Website
-            </h2>
+    <div class="hero-head">
+      <header class="navbar is-fixed-top is-transparent is-dark">
+        <div class="container">
+          <div class="navbar-brand">
+            <a class="navbar-item">
+            <p class="title is-5 has-text-white">Junead Khan</p>
+            </a>
+            <span class="navbar-burger" data-target="navbarMenuHeroC">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+          </div>
+          <div id="navbarMenuHeroC" class="navbar-menu">
+            <div class="navbar-end">
+              <a class="navbar-item is-active">
+                Home
+              </a>
+              <a class="navbar-item">
+                Resume
+              </a>
+              <a class="navbar-item">
+                Projects
+              </a>
+              <a class="navbar-item">
+              Contact
+             </a>
+              <span class="navbar-item">
+            <div class="field is-grouped">
+                <p class="control">
+                <button class="button">
+                <span class="icon">
+                  <i class="fa fa-github"></i>
+                </span>
+                <span>GitHub</span>
+              </button>
+                </p>
+                <p class="control">
+                    <button class="button is-info">
+                    <span class="icon" style="color: white;">
+                    <i class="fa fa-linkedin"></i>
+                    </span>
+                    <span>LinkedIn</span>
+                </button>
+                </p>
+            </div>
+              </span>
+            </div>
+          </div>
         </div>
-    </div>
-    </section>
+      </header>
+    `
+}
+
+export const heroSection = function() {
+    return `
+    <section class="hero is-medium is-link">
+  <div class="hero-body">
+    <p class="title">
+      Medium hero
+    </p>
+    <p class="subtitle">
+      Medium subtitle
+    </p>
+  </div>
+</section>
     `
 }
 
